88d631d2e3c2e58f32ed50eb3c1eea62eded6d57
edoszhan <yersultanofficial@gmail.com>
2023-07-31 15:54:56 +0900

    firebase storage check; potentially photoURLs and postFiles can be stored there

--- screens/home/TodoInformation.js
+++ screens/home/TodoInformation.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { storage } from '../../config/firebase';
+import { Image } from 'react-native';
+import { ref } from 'firebase/storage';
+import { getDownloadURL } from 'firebase/storage';
+
 
 export default function TodoInformation() {
+    const [imageUrl, setImageUrl] = useState(null); // State to store the image URL
+  
+    useEffect(() => {
+      // Function to fetch the image URL from Firebase Storage
+      const fetchImage = async () => {
+        try {
+        
+          const imageRef = ref(storage, '/6ede60dd5eafe20167423aace3ef378d.jpeg');
+          const url = await getDownloadURL(imageRef);
+          setImageUrl(url);
+        } catch (error) {
+          console.log('Error fetching image URL: ', error);
+        }
+      };
+  
+      fetchImage();
+    }, []);
+  
     return (
-        <View style={styles.container}> 
-            <Text style={styles.text}>TodoInformation</Text>
-        </View>
-    )
-}
+      <View style={styles.container}>
+        <Text style={styles.text}>TodoInformation</Text>
+        {imageUrl && <Image source={{ uri: imageUrl }} style={styles.image} />}
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingVertical: 100,
-        paddingHorizontal: 30,
-    },
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            },
+        text: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            },
+        image: {
+            width: 200,
+            height: 200,
+            marginTop: 20,
+            },
+        });
 
-    text: {
-        fontSize: 30,
-        fontWeight: 'bold',
-    },  
-});
+  
